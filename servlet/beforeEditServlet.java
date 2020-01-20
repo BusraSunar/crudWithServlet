124a5e0db3be8eb79d1c0fa9a291d6f1fa66595c
Büşra Sunar <54499947+BusraSunar@users.noreply.github.com>
2020-01-20 17:02:02 +0300

    Update beforeEditServlet.java

--- servlet/beforeEditServlet.java
+++ servlet/beforeEditServlet.java
@@ -25,72 +25,57 @@ public class beforeEditServlet extends HttpServlet {
 		try {
 			ArrayList<User> database=new ArrayList<User>();
 			
-	  		//HttpSession session = request.getSession();
+			//connect to database
 	  		Class.forName("com.mysql.jdbc.Driver");
-            Connection con = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3308/idk?autoReconnect=true&useSSL=false", "root", "");	
-            String[] idStr = request.getParameterValues("id");
-            Statement st=(Statement) con.createStatement();
-           
-            //HttpSession session = request.getSession();
-           // String data = (String) session.getAttribute("editData");
-      		//String id2 = data;
-            
-            //my object is to compare two sets if they are different than each other copy the arrays to resultsets
-      	  	String[] name = request.getParameterValues("name");
-      		String[] email = request.getParameterValues("email");
-      		
-      		ArrayList<User> list=new ArrayList<User>();
-      		for(int i=0;i<idStr.length;i++) {
-     	    	 User u= new User();
-	        	  u.setId(idStr[i]);
-	              u.setName(name[i]);
-	              u.setEmail(email[i]);
-	              list.add(u);
+            		Connection con = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3308/idk?autoReconnect=true&useSSL=false", "root", "");
+			//get array of ids
+            		String[] idStr = request.getParameterValues("id");
+           		Statement st=(Statement) con.createStatement();
+          
+			/*my object is to compare two sets if they are different than each other copy the arrays to resultsets
+			i will compare data and list, data is a list thats filled from teh database, list is from the inputs so 
+			when i compare list and data i will see the difference between them and assign list to data 
+			*/
+			
+			//get array of names and emails
+			String[] name = request.getParameterValues("name");
+			String[] email = request.getParameterValues("email");
+			
+			//fill list with our name, email, id arrays
+			ArrayList<User> list=new ArrayList<User>();
+			for(int i=0;i<idStr.length;i++) {
+				User u= new User();
+				u.setId(idStr[i]);
+				u.setName(name[i]);
+				u.setEmail(email[i]);
+				list.add(u);
+			}
+			
+			//fill data from the database
+		        String sql ="SELECT * FROM smth";
+			ResultSet rs=st.executeQuery(sql);
+			while(rs.next()){
+				User u= new User();
+				u.setId(rs.getString("id"));
+			        u.setName(rs.getString("name"));
+				u.setEmail(rs.getString("email"));
+				database.add(u);
+		        }
+			PreparedStatement stmt=null;
+			//compare data and list and if there is a difference assign list to data 
+			for(int i=0;i<list.size();i++) {
+				if(database.get(i).getId().equals(list.get(i).getId())) {
+					if(!database.get(i).getName().equals(list.get(i).getName()) || !database.get(i).getEmail().equals(list.get(i).getEmail())) {
+						String update="UPDATE smth SET name=?, email=? WHERE id='" +database.get(i).getId() +"'";
+						stmt = (PreparedStatement) con.prepareStatement(update);
+						stmt.setString(1,list.get(i).getName());
+						stmt.setString(2,list.get(i).getEmail());
+						stmt.executeUpdate();
+					}
+				}
+			    }
 
-       	 
-     	    }
-      	    String sql ="SELECT * FROM smth";
-      	    ResultSet rs=st.executeQuery(sql);
-      	    while(rs.next()){
-      	    	 User u= new User();
-	        	  u.setId(rs.getString("id"));
-	              u.setName(rs.getString("name"));
-	              u.setEmail(rs.getString("email"));
-	              database.add(u);
-
-        	 
-      	    }
-      	  PreparedStatement stmt=null;
-      	    
-      	    for(int i=0;i<list.size();i++) {
-      	    	if(database.get(i).getId().equals(list.get(i).getId())) {
-      	    		if(!database.get(i).getName().equals(list.get(i).getName()) || !database.get(i).getEmail().equals(list.get(i).getEmail())) {
-      	    			String update="UPDATE smth SET name=?, email=? WHERE id='" +database.get(i).getId() +"'";
-      	    			stmt = (PreparedStatement) con.prepareStatement(update);
-      	    			stmt.setString(1,list.get(i).getName());
-      	    			stmt.setString(2,list.get(i).getEmail());
-      	    			stmt.executeUpdate();
-      	    		}
-      	    	}
-      	    }
-  			
-
-
-      		/*PreparedStatement stmt=null;
-  
-      		
-  			String update="UPDATE smth SET name=?, email=? WHERE id='" +idStr +"'";
-  			stmt = (PreparedStatement) con.prepareStatement(update);
-  			//stmt.setString(1,name);
-  			//stmt.setString(2,email);
-  			stmt.executeUpdate();
-  			//session.invalidate();
-  			 * */
-  			 
   			response.sendRedirect("searchServlet");
-
-
-  		
 	}catch(Exception e) {}
 	}
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
