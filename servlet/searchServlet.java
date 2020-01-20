3fe72da63c08265e079a62807a6bfdd174dc204e
Büşra Sunar <54499947+BusraSunar@users.noreply.github.com>
2020-01-20 17:22:32 +0300

    Update searchServlet.java

--- servlet/searchServlet.java
+++ servlet/searchServlet.java
@@ -4,11 +4,7 @@ import java.io.IOException;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -20,39 +16,45 @@ import java.sql.*;
 @WebServlet("/searchServlet")
 public class searchServlet extends HttpServlet {
 
+	private static final long serialVersionUID = 1L;
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		ArrayList<User> list=new ArrayList<User>();
 		try{
+			//if the user logged in show the page 
 			if(request.getSession().getAttribute("loginYapildi").equals(true)) {
-		     Class.forName("com.mysql.jdbc.Driver");
-		           Connection con = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3308/idk?autoReconnect=true&useSSL=false", "root", "");
-		           Statement st=(Statement) con.createStatement();
-		           String query = request.getParameter("q");//bu search inputun name i 
-		          
-				   String sql="";
-					if(query!=null){
-						sql = "SELECT * FROM smth WHERE name LIKE '%"+query+"%' OR email LIKE '%"+query+"%'";
-					}else{
-						sql ="SELECT * FROM smth";
-					}
-		           ResultSet rs=st.executeQuery(sql);
-		           while(rs.next()){
-		              User u= new User();
-		        	  u.setId(rs.getString("id"));
-		              u.setName(rs.getString("name"));
-		              u.setEmail(rs.getString("email"));
-		              list.add(u);
-
-		           }
-		           
-		           request.setAttribute("data", list);
-		           request.getRequestDispatcher("table.jsp").forward(request, response);
+				
+				//connect to database
+				Class.forName("com.mysql.jdbc.Driver");
+	            Connection con = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3308/idk?autoReconnect=true&useSSL=false", "root", "");
+	            Statement st=(Statement) con.createStatement();
+	            
+	            //get input (if there is) from the search bar
+	            String query = request.getParameter("searchBar");
+	          
+	            //to display the table look if search bar is empty or not
+			    String sql="";
+				if(query!=null){
+					sql = "SELECT * FROM smth WHERE name LIKE '%"+query+"%' OR email LIKE '%"+query+"%'";
+				}else{
+					sql ="SELECT * FROM smth";
+				}
+	            ResultSet rs=st.executeQuery(sql);
+	            
+	            //get the results from the executeQuery method
+	            while(rs.next()){
+	            	User u= new User();
+	            	u.setId(rs.getString("id"));
+	            	u.setName(rs.getString("name"));
+	            	u.setEmail(rs.getString("email"));
+	            	list.add(u);
+	            }
+	           //set the data variable to list to show our table in table.jsp
+	            request.setAttribute("data", list);
+	            request.getRequestDispatcher("table.jsp").forward(request, response);
 			}else {
 				response.sendRedirect("index.jsp");
-
-				
 			}
-		           //rd.forward(request, response);
+		          
 		}
 		catch(Exception e){}
 	
@@ -61,36 +63,41 @@ public class searchServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		ArrayList<User> list=new ArrayList<User>();
 		try{
+			//if the user logged in show the page 
 			if(request.getSession().getAttribute("loginYapildi").equals(true)) {
-		     Class.forName("com.mysql.jdbc.Driver");
-		           Connection con = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3308/idk?autoReconnect=true&useSSL=false", "root", "");
-		           Statement st=(Statement) con.createStatement();
-		           String query = request.getParameter("q");//bu search inputun name i 
-		          
-				   String sql="";
-					if(query!=null){
-						sql = "SELECT * FROM smth WHERE name LIKE '%"+query+"%' OR email LIKE '%"+query+"%'";
-					}else{
-						sql ="SELECT * FROM smth";
-					}
-		           ResultSet rs=st.executeQuery(sql);
-		           while(rs.next()){
-		              User u= new User();
-		        	  u.setId(rs.getString("id"));
-		              u.setName(rs.getString("name"));
-		              u.setEmail(rs.getString("email"));
-		              list.add(u);
-
-		           }
-		           
-		           request.setAttribute("data", list);
-		           request.getRequestDispatcher("table.jsp").forward(request, response);
-			}else {
-				response.sendRedirect("index.jsp");
-
 				
-			}
-		           //rd.forward(request, response);
+				//connect to database
+				Class.forName("com.mysql.jdbc.Driver");
+	            Connection con = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3308/idk?autoReconnect=true&useSSL=false", "root", "");
+	            Statement st=(Statement) con.createStatement();
+	            
+	            //get input (if there is) from the search bar
+	            String query = request.getParameter("searchBar");
+	          
+	            //to display the table look if search bar is empty or not
+			    String sql="";
+				if(query!=null){
+					sql = "SELECT * FROM smth WHERE name LIKE '%"+query+"%' OR email LIKE '%"+query+"%'";
+				}else{
+					sql ="SELECT * FROM smth";
+				}
+	            ResultSet rs=st.executeQuery(sql);
+	            
+	            //get the results from the executeQuery method
+	            while(rs.next()){
+	            	User u= new User();
+	            	u.setId(rs.getString("id"));
+	            	u.setName(rs.getString("name"));
+	            	u.setEmail(rs.getString("email"));
+	            	list.add(u);
+	            }
+	           //set the data variable to list to show our table in table.jsp
+	            request.setAttribute("data", list);
+	            request.getRequestDispatcher("table.jsp").forward(request, response);
+		}else {
+			response.sendRedirect("index.jsp");
+		}
+		          
 		}
 		catch(Exception e){}
 	
